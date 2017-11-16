c291d4a8a79a14d5b77ea611f0750eebe83496f3
Matthieu MÉQUIGNON <matthieu@fwzte.xyz>
2017-11-16 13:09:03 +0100

    [REF] Class 'JSON' now instanciates from the right class dynamically.

--- src/org/ulco/JSON.java
+++ src/org/ulco/JSON.java
@@ -1,20 +1,22 @@
 package org.ulco;
 
+import java.lang.reflect.Constructor;
+
 public class JSON {
     static public GraphicsObject parse(String json) {
-        GraphicsObject o = null;
         String str = json.replaceAll("\\s+", "");
         String type = str.substring(str.indexOf("type") + 5, str.indexOf(","));
-        if (type.compareTo("square") == 0) {
-            o = new Square(str);
-        } else if (type.compareTo("rectangle") == 0) {
-            o = new Rectangle(str);
-        } else if (type.compareTo("circle") == 0) {
-            o = new Circle(str);
-        } else if (type.compareTo("group") == 0){
-            o = new Group(str);
+        type = "org.ulco." + type.substring(0, 1).toUpperCase() + type.substring(1);
+        try {
+            Class<?> graphics_object = Class.forName(type);
+            Constructor<?> constructor = graphics_object.getConstructor(String.class);
+            Object object = constructor.newInstance(str);
+            // return graphics_object.cast(object);
+            return (GraphicsObject) object;
         }
-        return o;
+        catch (Throwable t){
+            return null;
+        }
     }
 
     static public Group parseGroup(String json) {
